108cba1fcfe29d1cd4979754d71bba07764942b4
Alikadze <119809834+Alikadze@users.noreply.github.com>
2025-01-18 19:21:12 +0400

    added projects part to the portfolio

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, inject} from '@angular/core';
+import { PLATFORM_ID } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import {isPlatformBrowser} from "@angular/common";
+import {ProjectCardComponent} from "./project-card/project-card.component";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @Component({
     selector: 'app-projects',
-    imports: [],
+    imports: [
+        ProjectCardComponent,
+        MatTooltipModule,
+    ],
     templateUrl: './projects.component.html',
     styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements AfterViewInit {
+    PLATFORM_ID = inject(PLATFORM_ID);
 
+    ngAfterViewInit() {
+        gsap.registerPlugin(ScrollTrigger);
+
+        if (isPlatformBrowser(this.PLATFORM_ID)) {
+            gsap.utils.toArray('.main-container').forEach((container: any) => {
+                const sections = container.querySelectorAll('.panel'); // Fix the selector to match your HTML
+
+                gsap.to(sections, {
+                    xPercent: -100 * (sections.length - 1),
+                    ease: 'power1.inOut',
+                    scrollTrigger: {
+                        trigger: container,
+                        pin: true,
+                        scrub: 1,
+                        end: () => `+=${container.offsetWidth}`, // Adjust end dynamically
+                    }
+                });
+            });
+        }
+
+    }
 }
